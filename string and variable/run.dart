8afb5bb7dd101e3f33986c192c754e8ba3c76f60
Assalamalikum! Muhammad Anus Khan <muhammadanuskhan23@gmail.com>
2026-03-28 08:07:53 +0500

    Add files via upload

--- string and variable/run.dart
+++ string and variable/run.dart
@@ -1,3 +1,15 @@
 void main() {
   print("hello world"); //string => ""
+  print(2 + 3);
+  var text = "frod";
+  var age = 23;
+  print("home page");
+  print(age);
+  print("about page");
+  age=22;
+  print(age);
 }
+
+// Rules for making varible
+// 1. do not put number start at start of variable
+// 2. do not put special char in variable
