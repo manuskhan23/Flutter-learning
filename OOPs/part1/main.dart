0f756ca6fce5d741896470f1f21511d08bc3283d
Assalamalikum! Muhammad Anus Khan <muhammadanuskhan23@gmail.com>
2026-04-02 12:07:39 +0500

    Add files via upload

---
+++ OOPs/part1/main.dart
@@ -0,0 +1,15 @@
+void main() {
+  Human obj = Human();
+  print(obj);
+}
+
+class Human {
+  String name = "Anus";
+  num age = 14;
+  eat() {}
+  sleep() {}
+  walk() {}
+  run() {}
+  talk() {}
+  speak() {}
+}
